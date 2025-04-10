167db611f630761df40df421fea1f04d257dcfd3
pimentellima <matheus_dere@hotmail.com>
2025-04-10 12:19:52 -0300

    ref: alterado schema

--- apps/api/src/drizzle/schema.ts
+++ apps/api/src/drizzle/schema.ts
@@ -1,6 +1,28 @@
-import { pgTable, serial, varchar } from 'drizzle-orm/pg-core'
+import {
+    boolean,
+    date,
+    numeric,
+    pgTable,
+    serial,
+    text,
+    varchar,
+} from 'drizzle-orm/pg-core'
 
 export const users = pgTable('users', {
     id: serial('id').primaryKey(),
-    name: varchar('name', { length: 255 }),
+    name: text('name'),
+    waId: text('wa_id').unique(),
+    email: text('email'),
+    emailVerified: boolean('email_verified').default(false),
+})
+
+export const statements = pgTable('statements', {
+    id: serial('id').primaryKey(),
+    userId: serial('user_id')
+        .references(() => users.id, { onDelete: 'cascade' })
+        .notNull(),
+    description: text('description'),
+    date: date('date'),
+    amount: numeric('amount', { precision: 10, scale: 2 }),
+    createdAt: text('created_at').default('now()'),
 })
